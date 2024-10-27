f58e1804831bd01b58465a87ab287c49934a2c10
unknown <hoangziet22@gmail.com>
2024-10-27 15:55:47 +0700

    update code and add requirement

--- SOSdetector.py
+++ SOSdetector.py
@@ -9,7 +9,7 @@ class SOSdetector:
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
-        self.gesture_stage = 0
+        self.current_stage = 0
         self.start_time = time.time()
         self.end_time = time.time()
         
@@ -98,36 +98,36 @@ class SOSdetector:
 
     def detect_hand_signal(self, hand_landmarks):
         """Detect SOS hand signal"""
-        if self.gesture_stage == 0 and self._stage1(hand_landmarks):
-            self.gesture_stage = 1
-            print("Stage 1")
+        if self.current_stage == 0 and self._stage1(hand_landmarks):
+            self.current_stage = 1
+            # print("Stage 1")
 
-        if self.gesture_stage == 1 and self._stage2(hand_landmarks):
-            self.gesture_stage = 2
+        if self.current_stage == 1 and self._stage2(hand_landmarks):
+            self.current_stage = 2
             self.start_time = time.time()
-            print("Stage 2")
-            print(self.start_time)
+            # print("Stage 2")
+            # print("Start Time: ", self.start_time)
         
-        if self.gesture_stage == 2 and self._stage3(hand_landmarks):
-            self.gesture_stage = 3
-            print("Stage 3")
-        
-        if self.gesture_stage == 3 and self._stage2(hand_landmarks):
-            self.gesture_stage = 4
+        if self.current_stage == 2 and self._stage3(hand_landmarks):
+            self.current_stage = 3
+            # print("Stage 3")
+                
+        if self.current_stage == 3 and self._stage2(hand_landmarks):
+            self.current_stage = 4
             self.end_time = time.time()
-            print("Stage 4")
-            print(self.end_time)
+            # print("Stage 4")
+            # print("End Time: ", self.end_time)
 
     
-        if self.gesture_stage == 4:
+        if self.current_stage == 4:
             if self.end_time - self.start_time < 3:
                 return "SOS DETECTION"
             else:
-                self.gesture_stage = 0
+                self.current_stage = 0
 
         # Reset stages if time exceeds 3 seconds to avoid false positives
         if time.time() - self.start_time > 3:
-            self.gesture_stage = 0
+            self.current_stage = 0
             
         return "No SOS"
     
@@ -156,7 +156,7 @@ class SOSdetector:
 
                     if signal == "SOS DETECTION":
                         self._save_image(image)
-                        self.gesture_stage = 0  # Reset stages after capture
+                        self.current_stage = 0  # Reset stages after capture
 
             # Show results
             cv2.imshow('Hand Signal Detection', image)
